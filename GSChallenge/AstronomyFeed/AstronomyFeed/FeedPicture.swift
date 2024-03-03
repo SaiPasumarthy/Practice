15a7403c9bf53a019361a1e4f19be5366245d4fc
saipasumarthy <saipasumarthy009@gmail.com>
2024-03-03 16:13:19 +0530

    Created GS challenge project

---
+++ GSChallenge/AstronomyFeed/AstronomyFeed/FeedPicture.swift
@@ -0,0 +1,15 @@
+//
+//  FeedPicture.swift
+//  AstronomyFeed
+//
+//  Created by Sai Pasumarthy on 03/03/24.
+//
+
+import Foundation
+
+public struct FeedPicture {
+    let date: String
+    let explanation: String
+    let title: String
+    let url: URL
+}
