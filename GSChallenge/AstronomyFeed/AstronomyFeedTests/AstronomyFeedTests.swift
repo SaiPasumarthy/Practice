e57c39d9af0749313265ba1ac8934a1785e484a9
saipasumarthy <saipasumarthy009@gmail.com>
2024-03-10 20:11:40 +0530

    Refactored the tests. Moved the spy as test nested class. Make common test make SUT function

--- GSChallenge/AstronomyFeed/AstronomyFeedTests/AstronomyFeedTests.swift
+++ GSChallenge/AstronomyFeed/AstronomyFeedTests/AstronomyFeedTests.swift
@@ -6,9 +6,11 @@
 //
 
 import XCTest
+
 class RemoteFeedLoader {
     private let client: HTTPClient
     private let url: URL
+    
     init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
@@ -23,30 +25,37 @@ protocol HTTPClient {
     func get(from url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    var requestedURL: URL?
-    
-    func get(from url: URL) {
-        requestedURL = url
-    }
-}
-
 final class AstronomyFeedTests: XCTestCase {
 
     func test_init_doesnotRequestDataFromURL() {
-        let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(url: URL(string: "https://a-url.com")!, client: client)
+        let (_, client) = makeSUT()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
-        let client = HTTPClientSpy()
-        let loader = RemoteFeedLoader(url: url, client: client)
+        let (sut, client) = makeSUT(url: url)
 
-        loader.load()
+        sut.load()
         
         XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    // MARK: - Helpers
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            requestedURL = url
+        }
+    }
+    
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        
+        return (sut, client)
     }
 }
