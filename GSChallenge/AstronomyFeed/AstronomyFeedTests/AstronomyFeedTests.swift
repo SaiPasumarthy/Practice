a5c4aef242fcf27c6dc5aa0c8a284b08b37b7f49
saipasumarthy <saipasumarthy009@gmail.com>
2024-03-10 19:53:21 +0530

    RemoteFeedLoader requested the URL when load Make HTTPClient as protocol.

--- GSChallenge/AstronomyFeed/AstronomyFeedTests/AstronomyFeedTests.swift
+++ GSChallenge/AstronomyFeed/AstronomyFeedTests/AstronomyFeedTests.swift
@@ -7,19 +7,43 @@
 
 import XCTest
 class RemoteFeedLoader {
+    private let client: HTTPClient
+    init(client: HTTPClient) {
+        self.client = client
+    }
     
+    func load() {
+        client.get(from: URL(string: "https://a-url.com")!)
+    }
 }
 
-class HTTPClient {
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 final class AstronomyFeedTests: XCTestCase {
 
     func test_init_doesnotRequestDataFromURL() {
-        let client = HTTPClient()
-        _ = RemoteFeedLoader()
+        let client = HTTPClientSpy()
+        _ = RemoteFeedLoader(client: client)
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClientSpy()
+        let loader = RemoteFeedLoader(client: client)
+
+        loader.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 }
