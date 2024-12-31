bff15f82a4d1c4d0a8f30761c50df9b220225ed4
saipasumarthy <saipasumarthy009@gmail.com>
2024-12-31 15:36:30 +0530

    Finish URL loading when observing requests to make sure all URL requests are finished before test method returns.

--- GSChallenge/AstronomyFeed/AstronomyFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ GSChallenge/AstronomyFeed/AstronomyFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -187,22 +187,21 @@ final class URLSessionHTTPClientTests: XCTestCase {
         }
         
         override class func canonicalRequest(for request: URLRequest) -> URLRequest {
-            requestObserver?(request)
             return request
         }
         
         override func startLoading() {
-            guard let stub = URLProtocolStub.stub else {
-                return
+            if let observer = URLProtocolStub.requestObserver {
+                client?.urlProtocolDidFinishLoading(self)
+                return observer(request)
             }
-            
-            if let error = stub.error {
+            if let error = URLProtocolStub.stub?.error {
                 client?.urlProtocol(self, didFailWithError: error)
             }
-            if let response = stub.response {
+            if let response = URLProtocolStub.stub?.response {
                 client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
             }
-            if let data = stub.data {
+            if let data = URLProtocolStub.stub?.data {
                 client?.urlProtocol(self, didLoad: data)
             }
             client?.urlProtocolDidFinishLoading(self)
